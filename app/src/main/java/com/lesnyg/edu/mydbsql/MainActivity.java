ae935c4475711d4545fc9ea2df0097216d6583f8
이선영 <lsy79@hanmail.net>
2018-11-02 10:53:28 +0900

    commit1

--- app/src/main/java/com/lesnyg/edu/mydbsql/MainActivity.java
+++ app/src/main/java/com/lesnyg/edu/mydbsql/MainActivity.java
@@ -1,5 +1,6 @@
 package com.lesnyg.edu.mydbsql;
 
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
@@ -7,6 +8,7 @@ import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity implements OnClickListener {
     MyDBOpenHelper dbHelper;
@@ -22,6 +24,12 @@ public class MainActivity extends AppCompatActivity implements OnClickListener {
 
         Button buttoninsert = findViewById(R.id.buttoninsert);
         buttoninsert.setOnClickListener(this);
+        Button buttonread = findViewById(R.id.buttonread);
+        buttonread.setOnClickListener(this);
+        Button buttonupdata = findViewById(R.id.buttonupdata);
+        buttonupdata.setOnClickListener(this);
+        Button buttondele = findViewById(R.id.buttondele);
+        buttondele.setOnClickListener(this);
 
 
     }
@@ -34,7 +42,35 @@ public class MainActivity extends AppCompatActivity implements OnClickListener {
         String country = editTextcountry.getText().toString();
         String capital = editTextcapital.getText().toString();
 
-        mdb.execSQL("INSERT INTO awe_country VALUES(null,'"+country+"','"+capital+"');");
+        TextView tvResult = findViewById(R.id.textViewResult);
+        int id;
+        switch (v.getId()){
+            case R.id.buttoninsert:
+                mdb.execSQL("INSERT INTO awe_country VALUES(null,'"+country+"','"+capital+"');");
+                break;
+            case R.id.buttonread:
+
+                String query = "SELECT * FROM awe_country";  //order by _id desc
+                Cursor cursor = mdb.rawQuery(query, null);
+                String str = "";
+                while (cursor.moveToNext()) {
+                    id = cursor.getInt(0);
+                    country = cursor.getString(cursor.getColumnIndex("country"));
+                    capital = cursor.getString(2);
+                    str += (id + " : " + country + " - " + capital + "\n");
+                    tvResult.setText(str);
+                    }
+                break;
+            case R.id.buttonupdata:
+                String updata ="UPDATE awe_country SET capital='"+capital+"' WHERE country='"+country+"'";
+                mdb.execSQL(updata);
+                break;
+            case R.id.buttondele:
+                String dele = "DELETE FROM awe_country WHERE country='"+country+"'";
+                mdb.execSQL(dele);
+                break;
+        }
+
 
     }
 }
